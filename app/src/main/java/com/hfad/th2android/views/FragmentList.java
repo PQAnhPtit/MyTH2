e52d6340cde3e3f0f3e2d5740b6971317dad26a7
PQAnhPtit <116435372+PQAnhPtit@users.noreply.github.com>
2023-04-26 19:18:27 +0700

    commit

---
+++ app/src/main/java/com/hfad/th2android/views/FragmentList.java
@@ -0,0 +1,30 @@
+package com.hfad.th2android.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hfad.th2android.R;
+
+import java.util.List;
+
+public class FragmentList extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/hfad/th2android/views/FragmentSearch.java
@@ -0,0 +1,31 @@
+package com.hfad.th2android.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hfad.th2android.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FragmentSearch extends Fragment {
+    private EditText edFrom, edTo;
+    private Button btnSearch, btnStatistic;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search,container,false);
+    }
+}
